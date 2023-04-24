6f78f780f32de538adc3bd90ef79ac79d5d3312b
Murod-de <murad101693@gmail.com>
2023-04-24 12:17:22 +0500

    fixed project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App flex gap-5">
+      <h1 className="text-textColor">Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      textColor: 'rgba(58, 53, 65, 0.38)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
