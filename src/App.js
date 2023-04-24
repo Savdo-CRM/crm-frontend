ac759868a0f9d4ba366141f08e48350a5ed836b1
Murod-de <murad101693@gmail.com>
2023-04-24 12:23:30 +0500

    fixed project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 
 function App() {
   return (
-    <div className="App flex gap-5">
-      <h1 className="text-textColor">Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
+    <div className="App">
+      <h1 className="text-textColor p-5">Hello World</h1>
     </div>
   );
 }
